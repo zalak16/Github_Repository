3eaff72ce5fb09f3f2392c3723b230625b8b164e
zalak <zalak16@gmail.com>
2015-04-10 13:30:44 -0700

    Network motif local copy 4/10

---
+++ Local Copy Network Motif/src/test/java/edu/uw/nemo/NemoControllerTest.java
@@ -0,0 +1,19 @@
+package edu.uw.nemo;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+import static junit.framework.Assert.assertEquals;
+
+public class NemoControllerTest {
+
+    @Test
+    public void assertValidMotifCount() {
+        NemoController target = new NemoController(/*new Parser(), new ESUGen(), new NautyLabeler(), new DirectCalculator()*/);
+       // Map<String, List<Map.Entry<String, Long>>> actual = target.extract("full_scere_20140427.csv", 3);
+        Map<String, List<Map.Entry<String, Long>>> actual = target.extract("InputGraph3.csv", 3);
+   //     assertEquals(23, actual.size());
+    }
+
+}
