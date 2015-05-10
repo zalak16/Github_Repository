2929fdaa4fd3526a5458d0860ebda132df61eac6
zalak <zalak16@gmail.com>
2015-05-10 01:52:59 -0700

    compile file for mapreduce

--- Local Copy Network Motif/compile.sh
+++ Local Copy Network Motif/compile.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
- javac -cp `hadoop classpath`:.  src/main/java/edu/uw/nemo/model/AdjacentVertexWithEdge.java src/main/java/edu/uw/nemo/model/AdjacencyMapping.java src/main/java/edu/uw/nemo/model/Mapping.java src/main/java/edu/uw/nemo/io/Parser.java src/main/java/edu/uw/nemo/motifSignificant/mapreduce/SwitchingAlgorithmGenerateGraphMapper.java src/main/java/edu/uw/nemo/motifSignificant/mapreduce/SwitchingAlgorithmGenerateGraphReducer.java src/main/java/edu/uw/nemo/motifSignificant/mapreduce/GraphGeneratorJob.java src/main/java/edu/uw/nemo/motifSignificant/mapreduce/NemoRandomGraphMain.java 
- 
-  jar -cvf nemo.jar src/main/java/edu/uw/nemo/model/*.class src/main/java/edu/uw/nemo/io/Parser.class src/main/java/edu/uw/nemo/motifSignificant/mapreduce/*.class
- 
+echo "Compiling"
+
+javac -cp `hadoop classpath`:.  edu/uw/nemo/model/AdjacentVertexWithEdge.java edu/uw/nemo/model/AdjacencyMapping.java edu/uw/nemo/model/Mapping.java edu/uw/nemo/io/Parser.java edu/uw/nemo/motifSignificant/mapreduce/SwitchingAlgorithmGenerateGraphMapper.java edu/uw/nemo/motifSignificant/mapreduce/SwitchingAlgorithmGenerateGraphReducer.java edu/uw/nemo/motifSignificant/mapreduce/GraphGeneratorJob.java edu/uw/nemo/motifSignificant/mapreduce/NemoRandomGraphMain.java
+
+echo "Compilation done"
+
+jar -cvf nemo.jar edu/uw/nemo/model/*.class edu/uw/nemo/io/Parser.class edu/uw/nemo/motifSignificant/mapreduce/*.class
+
+echo "copying"
+ cp nemo.jar edu/uw/nemo/motifSignificant/mapreduce/nemo.jar
+
+echo "changing directory"
+
+cd edu/uw/nemo/motifSignificant/mapreduce/
+
